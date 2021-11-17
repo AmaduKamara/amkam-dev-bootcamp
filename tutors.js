f5579197a87f4417676e49d0e109453ca37ad785
Amkam <amaduskamara@gmail.com>
2021-11-17 16:38:26 +0000

    Add tutors.js file for holding featured turors array of objects

---
+++ tutors.js
@@ -0,0 +1,9 @@
+const featuredTeatures = [
+  {
+    id: '1',
+    name: 'Amadu Kamara',
+    role: 'Software Developer',
+    description: 'I am a software developer',
+    image: 'Image goeas here',
+  },
+];
